42c9db14c29cf0cc5e9ba8872efd964ec3e2a5c8
Mahmoudelnagar5 <elnagarm852@gmail.com>
2024-08-29 14:27:38 +0300

    best seller item part 3

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -10,15 +10,14 @@ abstract class Styles {
   static const textStyle20 = TextStyle(
     fontSize: 20,
     fontWeight: FontWeight.normal,
-    fontFamily: kGtSectraFine,
   );
   static const textStyle30 = TextStyle(
     fontSize: 30,
     fontWeight: FontWeight.normal,
     fontFamily: kGtSectraFine,
   );
-  static const textStyle14 = TextStyle(
-    fontSize: 14,
+  static const textStyle15 = TextStyle(
+    fontSize: 15,
     fontWeight: FontWeight.normal,
   );
 
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,4 +1,6 @@
+import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/best_seller_list_view_item.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/featured_books_list_view.dart';
 import 'package:flutter/material.dart';
@@ -24,54 +26,6 @@ class HomeViewBody extends StatelessWidget {
           ),
           SizedBox(height: 20),
           BestSellerListViewItem(),
-        ],
-      ),
-    );
-  }
-}
-
-class BestSellerListViewItem extends StatelessWidget {
-  const BestSellerListViewItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 125,
-      child: Row(
-        children: [
-          AspectRatio(
-            aspectRatio: 2.5 / 4,
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                image: const DecorationImage(
-                  fit: BoxFit.fill,
-                  image: AssetImage(
-                    'assets/images/jangle.jpeg',
-                  ),
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(width: 30),
-          Column(
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.5,
-                child: const Text(
-                  'Harry Potter and the Goblet of Fire',
-                  style: Styles.textStyle20,
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                ),
-              ),
-              // SizedBox(height: 5),
-              // Text(
-              //   'Rudyard Kipling',
-              //   style: Styles.textStyle14,
-              // ),
-            ],
-          )
         ],
       ),
     );
