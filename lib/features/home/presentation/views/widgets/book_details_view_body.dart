081869b8dc287b245555b805fc68cbee4368ebc0
Mahmoudelnagar5 <elnagarm852@gmail.com>
2024-08-29 22:22:41 +0300

    add title and subtitle to details

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,6 +1,9 @@
+import 'package:bookly_app/features/home/presentation/views/widgets/book_rating.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_item.dart';
 import 'package:flutter/material.dart';
+
+import '../../../../../core/utils/styles.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
@@ -11,13 +14,32 @@ class BookDetailsViewBody extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30.0),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           const CustomBookDetailsAppBar(),
           Padding(
             padding: EdgeInsets.symmetric(
-              horizontal: width * 0.17,
+              horizontal: width * 0.2,
             ),
             child: const CustomBookImage(),
+          ),
+          const SizedBox(height: 43),
+          Text(
+            'The Alchemist',
+            style: Styles.textStyle30.copyWith(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 6),
+          Opacity(
+            opacity: 0.7,
+            child: Text(
+              ' Paulo Coelho',
+              style: Styles.textStyle18.copyWith(
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
           ),
         ],
       ),
