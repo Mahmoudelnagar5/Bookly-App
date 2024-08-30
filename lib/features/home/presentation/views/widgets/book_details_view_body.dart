fa03898565abe3bb919e3cee56e6dd651b824334
Mahmoudelnagar5 <elnagarm852@gmail.com>
2024-08-30 15:17:21 +0300

    refactor book details view

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,67 +1,32 @@
-import 'package:bookly_app/features/home/presentation/views/widgets/book_rating.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/books_action.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/books_details_section.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_item.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/similar_books_list_view.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/similar_books_section.dart';
 import 'package:flutter/material.dart';
-import '../../../../../core/utils/styles.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    var width = MediaQuery.of(context).size.width;
-    return CustomScrollView(
+    return const CustomScrollView(
       slivers: [
         SliverFillRemaining(
           hasScrollBody: false,
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30.0),
+            padding: EdgeInsets.symmetric(
+              horizontal: 30.0,
+            ),
             child: Column(
               children: [
-                const CustomBookDetailsAppBar(),
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: width * 0.2,
-                  ),
-                  child: const CustomBookImage(),
+                CustomBookDetailsAppBar(),
+                BooksDetailsSection(),
+                SizedBox(
+                  height: 25,
                 ),
-                const SizedBox(height: 35),
-                Text(
-                  'The Alchemist',
-                  style: Styles.textStyle30.copyWith(
-                    fontWeight: FontWeight.bold,
-                  ),
+                SimilarBooksSection(),
+                SizedBox(
+                  height: 30,
                 ),
-                const SizedBox(height: 6),
-                Opacity(
-                  opacity: 0.7,
-                  child: Text(
-                    ' Paulo Coelho',
-                    style: Styles.textStyle18.copyWith(
-                      fontStyle: FontStyle.italic,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 18),
-                const BookRating(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                ),
-                const SizedBox(height: 30),
-                const BooksAction(),
-                const SizedBox(height: 40),
-                const Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'You can also like',
-                    style: Styles.textStyle18,
-                  ),
-                ),
-                const SizedBox(height: 20),
-                const SimilarBooksListView(),
-                const SizedBox(height: 40),
               ],
             ),
           ),
