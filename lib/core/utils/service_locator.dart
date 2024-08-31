a6eb59e9dfda214beccdfdf237bbb7fcdc60b08f
Mahmoudelnagar5 <elnagarm852@gmail.com>
2024-08-31 11:49:27 +0300

    setup service locator

--- lib/core/utils/service_locator.dart
+++ lib/core/utils/service_locator.dart
@@ -6,7 +6,11 @@ import 'package:get_it/get_it.dart';
 final getIt = GetIt.instance;
 
 void setupServiceLocator() {
-  getIt.registerSingleton<ApiServices>(ApiServices(Dio()));
+  getIt.registerSingleton<ApiServices>(
+    ApiServices(
+      Dio(),
+    ),
+  );
   getIt.registerSingleton<HomeRepoImpl>(
     HomeRepoImpl(
       getIt.get<ApiServices>(),
